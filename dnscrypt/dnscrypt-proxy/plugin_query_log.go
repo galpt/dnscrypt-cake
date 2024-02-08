c7dda1671bec74fcd7bc8b752ef342135ef9bbb1
nya~ <ara@0ms.dev>
2024-02-08 07:43:48 +0700

    fixed a potential ping spike issue
    The previous code logic caused ping spike to 1000ms+ somehow. This commit has been tested and is constantly giving 65 - 85ms DNS latency (tested with Nebulo android app). This improvement should affect CAKE too theoretically.

--- dnscrypt/dnscrypt-proxy/plugin_query_log.go
+++ dnscrypt/dnscrypt-proxy/plugin_query_log.go
@@ -116,6 +116,7 @@ func cakeBwRecovery() {
 		// check bandwidthh every second
 		// ------
 		// fast recovery uplink & downlink
+		time.Sleep(200 * time.Millisecond)
 		if bwUL < bwUL10 {
 			bwUL = bwUL10
 		} else if bwUL > bwUL10 && bwUL < bwUL30 {
@@ -151,9 +152,9 @@ func cakeBwNormalize() {
 		// it's possible for the bandwidth logic to fail to recover
 		// the bandwidth to the specified maxDL/maxUL.
 		// because of that, we want to normalize cake's bandwidth
-		// to maxDL/maxUL if it takes longer than 3 seconds to recover.
+		// to maxDL/maxUL if it takes longer than 5 seconds to recover.
 		// it should work well with cakeBwRecovery()
-		time.Sleep(3 * time.Second)
+		time.Sleep(5 * time.Second)
 
 		if bwUL < bwUL90 {
 			bwUL = bwUL90
@@ -188,8 +189,8 @@ func cakeResetRTT() {
 		// it's possible that RTT varies a lot too.
 		// this function will reset cake's rtt
 		// back to either "internetRTT" or "oceanicRTT"
-		// every 2 seconds.
-		time.Sleep(2 * time.Second)
+		// every 3 second.
+		time.Sleep(3 * time.Second)
 		if newRTT < internetRTT {
 			newRTT = internetRTT
 		}
@@ -294,8 +295,8 @@ func (plugin *PluginQueryLog) Eval(pluginsState *PluginsState, msg *dns.Msg) err
 		newRTT = requestDuration
 
 		// normalize RTT
-		if newRTT < metroRTT {
-			newRTT = metroRTT
+		if newRTT < internetRTT {
+			newRTT = internetRTT
 		}
 		if newRTT > satelliteRTT {
 			newRTT = satelliteRTT
